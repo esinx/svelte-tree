b7990e5ba4c52f1b33d2810d7149f1e4512935a4
mattthew <matthew.mcneely@gmail.com>
2021-12-06 16:41:00 -0500

    Add props.class attributes
    This allows the svg paths that draw the arrows to be styled. Here's an example of responding to global CSS (dark-mode) class changes
    
    ```
        <div class="parent">
        <Tree class="treeClass" tree={structureTree} let:node>
            {#if node.children}
                <div class="tree-node">{node.name}</div>
            {:else}
                <div class="label-node"><AbilityBug ability={getAbilityBySkill(node.id)}></AbilityBug></div>
            {/if}
        </Tree>
        </div>
    
    <style>
        .tree-node {
            line-height: 1.8;
        }
        :global(body.dark-mode) .parent :global(.treeClass) {
            fill: #bfc2c7;
        }
    </style>
    
    ```

--- src/tree.svelte
+++ src/tree.svelte
@@ -4,7 +4,7 @@
 </script>
 
 {#each tree as _node, i}
-  <TreeNode node={_node} index={i} let:node>
+  <TreeNode node={_node} index={i} let:node class={$$props.class}>
     <slot {node}>
       <div class="name">{node.name}</div>
     </slot>
